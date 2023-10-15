66a3a81cbdf95e3f9c79f1e75bf21766985d6c62
mohamedeliwa <mohamed.av11@gmail.com>
2023-10-15 23:27:17 +0300

    showing a progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use console;
+use dialoguer;
+use indicatif;
 use notify_rust::{Hint, Notification};
 use std::{
     io::{self, Write},
@@ -65,18 +68,14 @@ fn bool_answer_formatter() -> bool {
 */
 fn runner(minutes: u64) {
     let seconds = minutes * 60;
+
     let thread_join_handle = thread::spawn(move || {
-        for i in 1..(seconds + 1) {
+        let bar = indicatif::ProgressBar::new(seconds);
+        for _ in 0..seconds {
             thread::sleep(Duration::from_secs(1));
-            if i % 60 == 0 {
-                print!("{i}\n");
-            } else if i % 10 == 0 {
-                print!("{i}");
-            } else if i % 2 == 0 {
-                print!(".");
-            }
-            io::stdout().flush().unwrap();
+            bar.inc(1);
         }
+        bar.finish();
     });
     thread::sleep(Duration::from_secs(seconds));
     let _res = thread_join_handle.join();
