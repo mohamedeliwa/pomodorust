f7e40bd21b5fd3d0ebc7d9ced4f5019bcadbe0fb
mohamedeliwa <mohamed.av11@gmail.com>
2023-10-21 22:25:46 +0300

    a try to apply pause functionality

--- TODOs.md
+++ TODOs.md
@@ -2,6 +2,7 @@
 - [x] add notifcations functionality.
 - [x] make the counter output a digital timer that counts down
 - [ ] add the feature to pause/resume the timer `in progress...`
+    - [ ] add a channel to the thread so you can pause it (make it sleep till it resumes again)
 - [ ] make the app run inifite cycles of sessions and breaks until the user stops/pauses it.
 - [ ] make the pomodoro struct has its own impl of the runner function, and keeps track of what should run next \n
       the session or the break
--- src/pomodoro.rs
+++ src/pomodoro.rs
@@ -1,16 +1,27 @@
 // use console;
 use indicatif;
 use notify_rust::Notification;
-use std::{thread, time::Duration};
+use std::{
+    sync::mpsc::{self, Receiver, Sender},
+    thread,
+    time::Duration,
+};
 
+enum Action {
+    Resume,
+    Puase,
+}
 enum Period {
     Session,
     Pause,
 }
+
 pub struct Pomodoro {
     session: u64,
     pause: u64,
     next: Period,
+    tx: Option<Sender<Action>>,
+    rx: Option<Receiver<Action>>,
 }
 
 impl Pomodoro {
@@ -18,10 +29,13 @@ impl Pomodoro {
      * creates a new instance of the Pomodoro struct and initializes its state
      */
     pub fn new(session: u64, pause: u64) -> Pomodoro {
+        let (tx, rx) = mpsc::channel::<Action>();
         Pomodoro {
             session,
             pause,
             next: Period::Session,
+            tx: Some(tx),
+            rx: Some(rx),
         }
     }
     /**
@@ -34,22 +48,37 @@ impl Pomodoro {
             Period::Pause => self.pause * 60,
         };
 
-        let thread_join_handle = thread::spawn(move || {
-            let bar = indicatif::ProgressBar::new(seconds);
-            bar.set_style(indicatif::ProgressStyle::with_template("[{elapsed_precise}]").unwrap());
-            for _ in 0..seconds {
-                bar.inc(1);
-                thread::sleep(Duration::from_secs(1));
-            }
-            bar.finish();
-        });
+        let rx = self.rx.as_ref().unwrap();
+        // let thread_join_handle = thread::spawn(move || {
+        let bar = indicatif::ProgressBar::new(seconds);
+        bar.set_style(indicatif::ProgressStyle::with_template("[{elapsed_precise}]").unwrap());
+        for _ in 0..seconds {
+            bar.inc(1);
+            thread::sleep(Duration::from_secs(1));
+        }
+        bar.finish();
+        // });
 
-        let _res = thread_join_handle.join();
+        // let _res = thread_join_handle.join();
         self.notify();
         self.next = match self.next {
             Period::Session => Period::Pause,
             Period::Pause => Period::Session,
         };
+    }
+    /**
+     * pauses the timer
+     */
+    pub fn pause(&self) {
+        // transmits a pause message
+        self.tx.as_ref().unwrap().send(Action::Puase).unwrap();
+    }
+    /**
+     * resumes the timer
+     */
+    pub fn resume(&self) {
+        // transmits a resume message
+        self.tx.as_ref().unwrap().send(Action::Resume).unwrap();
     }
     /***
      * notifies the user that a period is elapsed
