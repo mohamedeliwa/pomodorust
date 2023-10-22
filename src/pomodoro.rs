d7636b6fd050e89d7318263c0a95e8928b2ac675
mohamedeliwa <mohamed.av11@gmail.com>
2023-10-22 14:51:25 +0300

    storing bar in the state of the pomodoro struct

--- src/pomodoro.rs
+++ src/pomodoro.rs
@@ -1,16 +1,17 @@
 // use console;
-use indicatif;
+use indicatif::{ProgressBar, ProgressStyle};
 use notify_rust::Notification;
 use std::{
-    sync::mpsc::{self, Receiver, Sender},
+    println,
+    // sync::mpsc::{self, Receiver, Sender},
     thread,
     time::Duration,
 };
 
-enum Action {
-    Resume,
-    Puase,
-}
+// enum Action {
+//     Resume,
+//     Puase,
+// }
 enum Period {
     Session,
     Pause,
@@ -20,8 +21,9 @@ pub struct Pomodoro {
     session: u64,
     pause: u64,
     next: Period,
-    tx: Option<Sender<Action>>,
-    rx: Option<Receiver<Action>>,
+    bar: Option<ProgressBar>,
+    // tx: Option<Sender<Action>>,
+    // rx: Option<Receiver<Action>>,
 }
 
 impl Pomodoro {
@@ -29,13 +31,14 @@ impl Pomodoro {
      * creates a new instance of the Pomodoro struct and initializes its state
      */
     pub fn new(session: u64, pause: u64) -> Pomodoro {
-        let (tx, rx) = mpsc::channel::<Action>();
+        // let (tx, rx) = mpsc::channel::<Action>();
         Pomodoro {
             session,
             pause,
             next: Period::Session,
-            tx: Some(tx),
-            rx: Some(rx),
+            bar: None,
+            // tx: Some(tx),
+            // rx: Some(rx),
         }
     }
     /**
@@ -43,43 +46,55 @@ impl Pomodoro {
      * prints passed time indicators to stdout
      */
     pub fn run(&mut self) {
-        let seconds = match self.next {
-            Period::Session => self.session * 60,
-            Period::Pause => self.pause * 60,
+        if self.bar.is_none() {
+            match self.next {
+                Period::Session => {
+                    let bar = ProgressBar::new(self.session * 60);
+                    bar.set_style(ProgressStyle::with_template("[{elapsed_precise}]").unwrap());
+                    self.bar = Some(bar)
+                }
+                Period::Pause => {
+                    let bar = ProgressBar::new(self.pause * 60);
+                    bar.set_style(ProgressStyle::with_template("[{elapsed_precise}]").unwrap());
+                    self.bar = Some(bar)
+                }
+            }
         };
-
-        let rx = self.rx.as_ref().unwrap();
-        // let thread_join_handle = thread::spawn(move || {
-        let bar = indicatif::ProgressBar::new(seconds);
-        bar.set_style(indicatif::ProgressStyle::with_template("[{elapsed_precise}]").unwrap());
-        for _ in 0..seconds {
+        let bar = self.bar.as_ref().expect("couldn't get self.bar ref");
+        let position = bar.position();
+        let length = bar.length().expect("failed to get bar length!");
+        for _ in position..length {
             bar.inc(1);
             thread::sleep(Duration::from_secs(1));
         }
-        bar.finish();
-        // });
 
-        // let _res = thread_join_handle.join();
         self.notify();
+        bar.finish();
         self.next = match self.next {
-            Period::Session => Period::Pause,
-            Period::Pause => Period::Session,
+            Period::Session => {
+                self.bar.take();
+                Period::Pause
+            }
+            Period::Pause => {
+                self.bar.take();
+                Period::Session
+            }
         };
     }
     /**
      * pauses the timer
      */
-    pub fn pause(&self) {
-        // transmits a pause message
-        self.tx.as_ref().unwrap().send(Action::Puase).unwrap();
-    }
+    // pub fn pause(&self) {
+    // transmits a pause message
+    // self.tx.as_ref().unwrap().send(Action::Puase).unwrap();
+    // }
     /**
      * resumes the timer
      */
-    pub fn resume(&self) {
-        // transmits a resume message
-        self.tx.as_ref().unwrap().send(Action::Resume).unwrap();
-    }
+    // pub fn resume(&self) {
+    // transmits a resume message
+    // self.tx.as_ref().unwrap().send(Action::Resume).unwrap();
+    // }
     /***
      * notifies the user that a period is elapsed
      */
