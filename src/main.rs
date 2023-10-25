2b6c3ec7b1ec90fe168efaa0e34e1abae42c879f
mohamedeliwa <mohamed.av11@gmail.com>
2023-10-26 01:34:31 +0300

    using the new key_handler and Pomodoro struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 use dialoguer;
-
+use std::sync::mpsc;
+mod key_handler;
 mod pomodoro;
-use pomodoro::Pomodoro;
+use pomodoro::{Actions, Pomodoro};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -15,24 +16,30 @@ struct Args {
 }
 
 fn main() {
+    let (tx, rx) = mpsc::channel::<Actions>();
+    let key_handle = key_handler::run(tx);
     let pomodoro = Args::parse();
+    let mut pomodoro = Pomodoro::new(pomodoro.session, pomodoro.pause, rx);
 
-    let mut pomodoro = Pomodoro::new(pomodoro.session, pomodoro.pause);
+    loop {
+        pomodoro.run();
+    }
 
-    let confirmation = dialoguer::Confirm::new()
-        .with_prompt("Start a session?")
-        .interact()
-        .unwrap();
-    if !confirmation {
-        return;
-    }
-    pomodoro.run();
-    let confirmation = dialoguer::Confirm::new()
-        .with_prompt("Start a break?")
-        .interact()
-        .unwrap();
-    if !confirmation {
-        return;
-    }
-    pomodoro.run();
+    // let confirmation = dialoguer::Confirm::new()
+    //     .with_prompt("Start a session?")
+    //     .interact()
+    //     .unwrap();
+    // if !confirmation {
+    //     return;
+    // }
+    // pomodoro.run();
+    // let confirmation = dialoguer::Confirm::new()
+    //     .with_prompt("Start a break?")
+    //     .interact()
+    //     .unwrap();
+    // if !confirmation {
+    //     return;
+    // }
+    // pomodoro.run();
+    key_handle.join().expect("failed to join key_handler!");
 }
